0af38762ce102016c67c45c492049420c8c1c3af
unk900t2 <stanislav.savolainen@mail.com>
2020-02-20 17:24:18 +0200

    backend update user online and offline status, if user leave page

--- backend/nodejs_proj1/server1.js
+++ backend/nodejs_proj1/server1.js
@@ -43,16 +43,23 @@ app.use(express.static('host_client'));
 
 var profileObject = undefined;
 
+var userConfirmTimer = 10000; //1minute inactivity
+var userTableName = "user_table1"
+
 
 // ================ load user profile ==========================
 
-app.get('/1', function (req, res) { 
-     res.status(301).redirect("/profile1.html");
+app.get('/1', function (req, res) {
+    res.status(301).redirect("/profile1.html");
 });
 
-app.get('/loadUserProfile', function (req, res, next) { 
+app.get('/loadUserProfile', function (req, res, next) {
 
     //make Session for special user special profile
+
+
+
+
 
     res.send(profileObject);
 
@@ -67,6 +74,25 @@ app.get('/confirmOnlineStatus', function (req, res, next) {
     //this ajax call after user is successfully logged in
 
     console.log("heartbeat check timestamp  : " + (new Date()).toString());
+
+    //check last online status, and set user offline if heartbeat not happen anymore
+
+    //let currentTime = timestampToString();
+
+    //get uset last logged time from database
+
+    let s_user_uuid = profileObject.user_uuid;
+    profileObject.last_online_date = timestampToString();
+    profileObject.user_is_online = 1; //user is online
+
+    knex(userTableName).update(profileObject).where({ user_uuid: s_user_uuid }).then(
+        () => { console.log("User data succesfully updated !"); return ("done") }
+    ).catch((e) => {
+        console.log(e)
+        return ("Failed")
+    })
+
+
 
     res.send("user UUID with session : 839048290 on device UUID : 859043584390 is online");
 
@@ -92,49 +118,49 @@ app.post('/authenticate', function (req, res, next) {
 
     console.log("Password : " + user_password + " sha256 crypted hash :" + password_hash);
 
-    var tablename = "user_table1";
+    // var tablename = "user_table1";
 
     let notfound = true;
 
-    knex(tablename).then(function (database_result) {
+    knex(userTableName).then(function (database_result) {
         //return JSON.stringify(database_result);
-       // res.send(JSON.stringify(database_result));
+        // res.send(JSON.stringify(database_result));
 
-       usersObjectArray = database_result;
+        usersObjectArray = database_result;
 
 
-       database_result.forEach( function ( element ) { 
+        database_result.forEach(function (element) {
 
-          // element.username
+            // element.username
 
-           //element.password_hash_sha256 
+            //element.password_hash_sha256 
 
-           if( element.username == user_login && element.password_hash_sha256 == password_hash ){
+            if (element.username == user_login && element.password_hash_sha256 == password_hash) {
 
-               notfound = false;
+                notfound = false;
 
-               profileObject = element; 
+                profileObject = element;
 
-             //  setTimeout(function(){ 
-                  
-                  // res.redirect('profile1.html');
+                //  setTimeout(function(){ 
 
-               // }, 250);  
+                // res.redirect('profile1.html');
 
-               res.send("found");
-               // res.redirect('profile1.html');
-           
+                // }, 250);  
+
+                res.send("found");
+                // res.redirect('profile1.html');
+
                 //res.status(301).redirect("/1")
             }
 
 
-       });
+        });
 
-       if( notfound ) profileObject = undefined;
+        if (notfound) profileObject = undefined;
 
-       if( notfound ) res.send("not_found");
+        if (notfound) res.send("not_found");
 
-  
+
 
     }).catch((e) => {
         console.log(e)
@@ -172,7 +198,7 @@ app.post('/registeruser', function (req, res, next) {
         console.log("Register (server time ) - timestamp : " + insert_register_date);
 
 
-        //ingore regularExpression / joiValidation / Sanitaze or what ever input validation at the moment
+        //ignore regularExpression / joiValidation / Sanitaze or whatever input validation at the moment
 
         /*
          row_id                | int(11)      | NO   | PRI | NULL    | auto_increment |
@@ -191,7 +217,7 @@ app.post('/registeruser', function (req, res, next) {
 
         //also duplicate check missing to not register same user 
 
-        var tablename = "user_table1";
+        //var tablename = "user_table1";
         var user_object = {
             user_uuid: insert_uuid,
             password_hash_sha256: insert_password_sha256,
@@ -205,8 +231,7 @@ app.post('/registeruser', function (req, res, next) {
 
         };
 
-        knex(tablename).insert(user_object).then(() => {
-            console.log("Inserted temperature sample")
+        knex(userTableName).insert(user_object).then(() => {
             return ("Insert succesful")
         })
             .catch((e) => {
@@ -260,6 +285,181 @@ function timestampToString() {
 }
 
 
+function getTZTime(tzOffset) {
+    local = new Date();
+    off = (local.getTimezoneOffset()) * 60 * 1000;
+    timeStamp = local.getTime() + off;
+    off += 1000 * 60 * 60 * tzOffset;
+    nd = new Date();
+    nd.setTime(timeStamp);
+    return (nd);
+}
+
+
+
+//========= user who don't response to heartbeat =================
+
+function dropInAcitivityUserOfflineSceduler() {
+
+    setInterval(function () {
+
+        let currentTime = timestampToString();
+
+        knex(userTableName).then(function (database_result) {
+
+
+
+            database_result.forEach(function (element) {
+
+                let lastOnlineDate = element.last_online_date.toString();
+
+                let myobj = JSON.stringify(element);
+                let pmyobj = JSON.parse(myobj);
+
+                //google.com => node js date difference in seconds
+                //https://stackoverflow.com/questions/2024198/how-many-seconds-between-two-dates
+
+                //Difference : -7194 >>> current 2020-02-20 13:21:14 last :Thu Feb 20 2020 15:21:08 GMT+0200 (EET)
+
+                // var a = new Date(getTZTime(2));
+                // var a = new Date();
+                // a.setTime( (new Date().getTime()) + 
+                // (a.getTimezoneOffset() * 2 * 60 * 1000) );
+                var a = new Date(timestampToString()); //.toLocaleDateString();
+
+
+
+                // var a = new Date();
+                //  var b = new Date(lastOnlineDate).toUTCString();
+                // var b = new Date ( lastOnlineDate  );
+                //var b = bb.toUTCString();
+                //var b = new Date("2020-02-20 14:14:19");
+                // var b = new Date(pmyobj.last_online_date);
+                // 2020-02-20T14:14:19.000Z
+                var modified_b = lastOnlineDate.replace("T", " ")
+                modified_b = modified_b.substring(modified_b.length - 4, 4);
+                modified_bb = pmyobj.last_online_date.substr(0, 4)
+                    + "-" + pmyobj.last_online_date.substr(5, 2)
+                    + "-" + pmyobj.last_online_date.substr(8, 2)
+                    + " " + pmyobj.last_online_date.substr(11, 2)
+                    + ":" + pmyobj.last_online_date.substr(14, 2)
+                    + ":" + pmyobj.last_online_date.substr(17, 2)
+
+                var b = new Date(modified_bb);
+
+
+                // b.setTime((new Date(lastOnlineDate).getTime()) + 
+                // (b.getTimezoneOffset() * 60 * 1000) + // local offset
+                // (1000 * 60 * 60 * 2));
+
+
+
+                //var c = new Date();
+                //  c.setTime((new Date().getTime()) + 
+                // (c.getTimezoneOffset() * 60 * 1000) + // local offset
+                // (1000 * 60 * 60 * 2));
+
+
+
+                // https://stackoverflow.com/questions/7971813/get-helsinki-local-time-regardless-of-local-time-zone 
+
+
+                /*
+                 var b = new Date();
+                 b.setFullYear(  parseInt( lastOnlineDate.substr( 0, 4  )  ) );
+                 b.setMonth( parseInt( lastOnlineDate.substr( 5, 2  )      ) );
+                 b.setDate( parseInt( lastOnlineDate.substr( 8, 2  )       ) );
+                 b.setHours( parseInt( lastOnlineDate.substr( 11, 2  )     ) );
+                 b.setMinutes( parseInt( lastOnlineDate.substr( 14, 2  )   ) );
+                 b.setSeconds(  parseInt(  lastOnlineDate.substr( 17, 2  ) ) );
+                 
+  
+                // var t1 = new Date(YYYY, MM, DD, 0, 0, 0, 0);
+                //var t2 = new Date(ZZZZ, NN, EE, 0, 0, 0, 0);
+                   var t1 = new Date(a.getFullYear(), a.getMonth(), a.getMonth(), a.getHours(), a.getMinutes(), a.getSeconds(), a.getMilliseconds());
+                  var t2 = new Date(b.getFullYear(), b.getMonth(), b.getMonth(), b.getHours(), b.getMinutes(), b.getSeconds(), b.getMilliseconds());
+  
+                  var dif = t1.getTime() - t2.getTime();
+  
+                  var Seconds_from_T1_to_T2 = dif / 1000;
+                  var Seconds_Between_Dates = Math.abs(Seconds_from_T1_to_T2);
+  
+  
+                  var difference = (a.getTime() - b.getTime() ) / 1000;
+  
+                  console.log("Difference : " + (difference + 5000) + " >>> current " + currentTime + " last :" + lastOnlineDate);
+  
+                  console.log(b);
+                  console.log(a);
+  
+                  console.log(Seconds_from_T1_to_T2 / 1000);
+                  console.log(Seconds_Between_Dates / 1000);
+                  */
+
+                //https://www.geeksforgeeks.org/how-to-calculate-the-number-of-days-between-two-dates-in-javascript/
+
+                var Difference_In_Time = a.getTime() - b.getTime();
+
+                // To calculate the no. of days between two dates 
+                var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+
+                var difference = (a.getTime() - b.getTime()) / 1000;
+
+                console.log("Difference : " + difference + " >>> current " + a + " last :" + b);
+                console.log(element.last_online_date);
+                // console.log(a);
+                console.log("database timestamp :" + b + "and modified bb : " + modified_bb);
+
+                // console.log(a.getTime());
+                console.log(timestampToString());
+                //console.log(b.getTime());
+                // console.log( a.getTime() - b.getTime() / 1000 );
+
+
+                console.log("DB last onlien :" + pmyobj.last_online_date);
+
+                // flag user offline , if he don't response "ConfirmOnlieStatus" ajax call
+                if (difference > 60 && typeof profileObject != 'undefined' ) {
+
+                    let s_user_uuid = profileObject.user_uuid;
+                    profileObject.user_is_online = 0; // user is offline
+
+                    knex(userTableName).update(profileObject).where({ user_uuid: s_user_uuid }).then(
+                        () => { console.log("User data succesfully updated !"); return ("done") }
+                    ).catch((e) => {
+                        console.log(e)
+                        return ("Failed")
+                    })
+
+
+
+                }
+
+
+            });
+
+
+
+        }).catch((e) => {
+            console.log(e)
+
+        });
+
+
+
+
+
+
+    }, userConfirmTimer);
+
+
+
+}
+
+
+//===== load user ==============================================
+
+
 
 //==============================================================
 
@@ -268,4 +468,6 @@ var server = app.listen(1111, function () {
     var hostport = server.address().port;
     console.log("Express server is listening");
     console.log("Port is: %s", hostport);
+
+    dropInAcitivityUserOfflineSceduler();
 });
