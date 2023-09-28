f4b22f69b9da78459091ff9ceae124af5965ef04
stanislavsavolainen <stanislav.savolainen@mail.com>
2023-09-28 22:03:34 +0300

    added Getters and Setters for each Product parameters

--- springboot/online_shop_project1_plan1/ProductModel1.java
+++ springboot/online_shop_project1_plan1/ProductModel1.java
@@ -46,56 +46,332 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import java.util.Date;
+
 @Entity
 @Table(name="shopProduct1")
 public class ProductModel1 {
+
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="rowId")
 	int id;
 
-	@Column(name="productName")
-	String productName;
-
-	@Column(name="productType")
-	int productType;
-
-	//.... toDO : all parameters correspond database table shopProduct1
-
-
-
 	public Integer getId() {
-		return id;
+		return this.id;
 	}
 
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	
+
+	//=====================================================
+
+    @Column(name="productName")
+	String productName;
+
 	public String getProductName() {
-		return productName;
+		return this.productName;
 	}
 
-	public void setProduct(String paramProductName) {
-		productName = paramProductName;
+	public void setProductName(String paramProductName) {
+		this.productName = paramProductName;
 	}
+
+	//=====================================================
+
+    @Column(name="productType")
+	int productType;
 
 	public Integer getProductType() {
-		return productType;
+		return this.productType;
 	}
 
 	public void setProductType(Integer paramProductType) {
-		productType = paramProductType;
+		this.productType = paramProductType;
 	}
 
-	//..... toDO : Write more getters and setters for each parameter described above
+	//=====================================================
+
+	@Column(name="productImageURL")
+	String productImageURL; 
+
+	public String getProductImageURL() {
+		return this.productImageURL;
+	}
+
+	public void setProductImageURL(String paramProductImageURL) {
+		this.productImageURL = paramProductImageURL;
+	}
+
+	//=====================================================
+
+	@Column(name="productDescription")	
+	String productDescription; 
+
+	public String getProductDescription() {
+		return this.productDescription;
+	}
+
+	public void setProductDescription(String paramProductDescription) {
+		this.productDescription = paramProductDescription;
+	}
+
+	//=====================================================
+
+	@Column(name="productPricePerUnit")
+	double productPricePerUnit;
+
+	public double getProductPricePerUnit() {
+		return this.productPricePerUnit;
+	}
+
+	public void setProductPricePerUnit(String paramProductPricePerUnit) {
+		this.productPricePerUnit = paramProductPricePerUnit;
+	}
+
+	//=====================================================
+
+	@Column(name="productQuantity")
+	int productQuantity;
+
+	public double getProductQuantity() {
+		return this.productQuantity;
+	}
+
+	public void setProductQuantity(Integer paramProductQuantity) {
+		this.productQuantity = paramProductQuantity;
+	}
 
 
+	//=====================================================
+
+	@Column(name="productQuantityOption")
+	String productQuantityOption;
+
+	public String getProductQuantityOption() {
+		return this.productQuantityOption;
+	}
+
+	public void setProductQuantityOption(String paramProductQuantityOption) {
+		this.productQuantityOption = paramProductQuantityOption;
+	}
+
+	//=====================================================
+
+	@Column(name="shippingInfo")
+	String shippingInfo;
+
+	public String getShippingInfo() {
+		return this.shippingInfo;
+	}
+
+	public void setShippingInfo(String paramShippingInfo) {
+		this.shippingInfo = paramShippingInfo;
+	}
+
+	//=====================================================
+
+	@Column(name="shippingPriceSameCountry")
+	double shippingPriceSameCountry;
+
+	public double getShippingPriceSameCountry() {
+		return this.shippingPriceSameCountry;
+	}
+
+	public void setShippingPriceSameCountry(double paramShippingPriceSameCountry) {
+		this.shippingPriceSameCountry = paramShippingPriceSameCountry;
+	}
+
+	//=====================================================
+
+	@Column(name="shippingPriceEurope")
+	double shippingPriceEurope;
+
+	public double getShippingPriceEurope() {
+		return this.shippingPriceEurope;
+	}
+
+	public void setShippingPriceEurope(double paramShippingPriceEurope) {
+		this.shippingPriceEurope = paramShippingPriceEurope;
+	}
+
+	//=====================================================
+
+	@Column(name="shippingPriceWorld")
+	double shippingPriceWorld;
+
+	public double getShippingPriceWorld() {
+		return this.shippingPriceWorld;
+	}
+
+	public void setShippingPriceWorld(double paramShippingPriceWorld) {
+		this.shippingPriceWorld = paramShippingPriceWorld;
+	}
+
+
+	//=====================================================
+
+	@Column(name="sellerUUID")
+ 	String sellerUUID;
+
+	public String getSellerUUID() {
+		return this.sellerUUID;
+	}
+
+	public void setSellerUUID(String paramSellerUUID) {
+		this.sellerUUID = paramSellerUUID;
+	}
+
+	//=====================================================
+
+	@Column(name="sellerQualityScore")
+	double sellerQualityScore;
+
+	public double getShippingPriceWorld() {
+		return this.sellerQualityScore;
+	}
+
+	public void setShippingPriceWorld(double paramSellerQualityScore) {
+		this.sellerQualityScore = paramSellerQualityScore;
+	}
+
+	//=====================================================
+
+	@Column(name="productQualityScore")
+	double productQualityScore;
+
+	public double getShippingPriceWorld() {
+		return this.productQualityScore;
+	}
+
+	public void setShippingPriceWorld(double paramProductQualityScore) {
+		this.productQualityScore = paramProductQualityScore;
+	}
+
+	//=====================================================
+
+	@Column(name="productFeedback")
+	String productFeedback; 
+
+	public String getProductFeedback() {
+		return this.productFeedback;
+	}
+
+	public void setProductFeedback(String paramProductFeedback) {
+		this.productFeedback = paramProductFeedback;
+	}
+
+	//=====================================================              
+
+	@Column(name="buyerUUID")
+	String buyerUUID; 
+
+	public String getBuyerUUID() {
+		return this.buyerUUID;
+	}
+
+	public void setbuyerUUID(String paramBuyerUUID) {
+		this.buyerUUID = paramBuyerUUID;
+	}
+
+	//=====================================================
+
+	@Column(name="buyerQualityScore")
+	double buyerQualityScore;
+
+	public double getBuyerQualityInfoVSProduct() {
+		return this.buyerQualityScore;
+	}
+
+	public void setBuyerQualityInfoVSProduct(double paramBuyerQualityScore) {
+		this.buyerQualityScore = paramBuyerQualityScore;
+	}
+
+	//=====================================================
+
+	@Column(name="buyerQualityInfoVSProduct")
+ 	String buyerQualityInfoVSProduct;
+
+	public String getBuyerQualityInfoVSProduct() {
+		return this.buyerQualityInfoVSProduct;
+	}
+
+	public void setBuyerQualityInfoVSProduct(String paramBuyerQualityInfoVSProduct) {
+		this.buyerQualityInfoVSProduct = paramBuyerQualityInfoVSProduct;
+	}
+
+	//===================================================== 
+
+	@Column(name="productCreated")
+	Date productCreated;
+
+	public Date getProductCreated() {
+		return this.productCreated;
+	}
+
+	public void setProductCreated(Date paramProductCreated) {
+		this.productCreated = paramProductCreated;
+	}
+
+	//=====================================================
+
+	@Column(name="productClosed")
+	Date productClosed;
+
+	public Date getProductClosed() {
+		return this.productClosed;
+	}
+
+	public void setProductClosed(Date paramProductClosed) {
+		this.productClosed = paramProductClosed;
+	}
+
+	//=====================================================
+
+	@Column(name="productWarrantyAndRefund")
+	String productWarrantyAndRefund;
+
+	public String getProductWarrantyAndRefund() {
+		return this.productWarrantyAndRefund;
+	}
+
+	public void setProductWarrantyAndRefund(String paramProductWarrantyAndRefund) {
+		this.productWarrantyAndRefund = paramProductWarrantyAndRefund;
+	}
+
+	//=====================================================
+	
+	@Column(name="productPublicVisible")
+	int productPublicVisible;
+
+	public Integer getProductPublicVisible() {
+		return this.productPublicVisible;
+	}
+
+	public void setProductPublicVisible(Integer paramId) {
+		this.productPublicVisible = paramId;
+	}
+
+	//=====================================================
+
+	@Column(name="adminInfo")
+	String adminInfo;
+
+	public String getAdminInfo() {
+		return this.adminInfo;
+	}
+
+	public void setAdminInfo(String paramAdminInfo) {
+		this.adminInfo = paramAdminInfo;
+	}
+
+	//=====================================================
 
 	public String getModelAsJSON(){
 		String result = "";
-		String localValue = "Model is not wotking, implementation not done"
+		String localValue = "Model is not wotking, implementation not done";
 		result += "\"info\":"+"\""+ localValue + "\""+"}";
 	}
 
